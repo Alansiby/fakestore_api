b350904c9bc09d0e7723418b5dde011acb571685
Alansiby <alansibyp2001@gmail.com>
2024-04-26 20:08:58 +0530

    Cart Screen

--- lib/controller/cart_screen_controller.dart
+++ lib/controller/cart_screen_controller.dart
@@ -8,6 +8,9 @@ import 'package:flutter/material.dart';
 class CartScreenController with ChangeNotifier {
   List<CartModel> cartList = [];
 
+  //at first the amount is zero
+  double totalAmount = 0;
+
   addToCart(ProductsListModel product, BuildContext context) {
     final isCarted =
         cartList.any((element) => element.product.id == product.id);
@@ -18,24 +21,39 @@ class CartScreenController with ChangeNotifier {
     } else {
       cartList.add(CartModel(product: product));
     }
-
+    calculateTM();
     notifyListeners();
   }
 
   deletFormCart(int index) {
     cartList.removeAt(index);
+    calculateTM();
     notifyListeners();
   }
 
   onIncrementQty(int index) {
     cartList[index].qty = cartList[index].qty + 1;
+    calculateTM();
     notifyListeners();
   }
 
   onDecrementQty(int index) {
     if (cartList[index].qty > 1) {
       cartList[index].qty = cartList[index].qty - 1;
+      calculateTM();
       notifyListeners();
     }
+  }
+
+//for calculating total amount
+
+  calculateTM() {
+    totalAmount = 0;
+    for (var element in cartList) {
+      //to calculate the amount of each product
+      totalAmount = totalAmount + (element.qty * (element.product.price ?? 0));
+    }
+    print("TOtal : $totalAmount");
+    notifyListeners();
   }
 }
--- lib/view/cart_screen/cart_screen.dart
+++ lib/view/cart_screen/cart_screen.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sort_child_properties_last
 
 import 'package:fakestore_api/controller/cart_screen_controller.dart';
 import 'package:flutter/material.dart';
@@ -16,100 +16,125 @@ class _CartScreenState extends State<CartScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(),
-        body: Consumer<CartScreenController>(
-          builder: (context, providerObj, child) {
-            return ListView.builder(
-                itemCount: providerObj.cartList.length,
-                itemBuilder: (context, index) => Container(
-                      child: Padding(
-                        padding:
-                            EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                        child: Container(
-                          width: double.infinity,
-                          decoration:
-                              BoxDecoration(color: Colors.white, boxShadow: [
-                            BoxShadow(
-                                offset: Offset(4, 6),
-                                blurRadius: 6,
-                                color: Colors.black.withOpacity(0.4))
-                          ]),
-                          child: Row(
-                            children: [
-                              Container(
-                                height: 100,
-                                width: 100,
-                                decoration: BoxDecoration(
-                                    image: DecorationImage(
-                                        image: NetworkImage(providerObj
-                                                .cartList[index]
-                                                .product
-                                                .image ??
-                                            ""))),
-                              ),
-                              Expanded(
-                                child: Column(
-                                  mainAxisSize: MainAxisSize.min,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    SizedBox(
-                                      width: 105,
-                                    ),
-                                    Text(
-                                      providerObj.cartList[index].product.title
-                                          .toString(),
-                                      maxLines: 3,
-                                      overflow: TextOverflow.ellipsis,
-                                    ),
-                                    SizedBox(
-                                      height: 20,
-                                    ),
-                                    Text(providerObj
-                                        .cartList[index].product.price
-                                        .toString())
-                                  ],
-                                ),
-                              ),
-                              Spacer(),
-                              Column(
+      appBar: AppBar(),
+      body: Consumer<CartScreenController>(
+        builder: (context, providerObj, child) {
+          return Column(
+            children: [
+              Expanded(
+                child: ListView.builder(
+                  itemCount: providerObj.cartList.length,
+                  itemBuilder: (context, index) => Container(
+                    child: Padding(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                      child: Container(
+                        width: double.infinity,
+                        decoration:
+                            BoxDecoration(color: Colors.white, boxShadow: [
+                          BoxShadow(
+                              offset: Offset(4, 6),
+                              blurRadius: 6,
+                              color: Colors.black.withOpacity(0.4))
+                        ]),
+                        child: Row(
+                          children: [
+                            Container(
+                              height: 100,
+                              width: 100,
+                              decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                      image: NetworkImage(providerObj
+                                              .cartList[index].product.image ??
+                                          ""))),
+                            ),
+                            Expanded(
+                              child: Column(
                                 mainAxisSize: MainAxisSize.min,
-                                crossAxisAlignment: CrossAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
-                                  InkWell(
-                                      onTap: () {
-                                        providerObj.deletFormCart(index);
-                                      },
-                                      child: Icon(Icons.delete)),
+                                  SizedBox(
+                                    width: 105,
+                                  ),
+                                  Text(
+                                    providerObj.cartList[index].product.title
+                                        .toString(),
+                                    maxLines: 3,
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
                                   SizedBox(
                                     height: 20,
                                   ),
-                                  Row(
-                                    children: [
-                                      InkWell(
-                                          onTap: () {
-                                            providerObj.onIncrementQty(index);
-                                          },
-                                          child: Icon(Icons.add)),
-                                      Center(
-                                        child: Text(providerObj
-                                            .cartList[index].qty
-                                            .toString()),
-                                      ),
-                                      InkWell(
-                                          onTap: () {
-                                            providerObj.onDecrementQty(index);
-                                          },
-                                          child: Icon(Icons.remove))
-                                    ],
-                                  )
+                                  Text(providerObj.cartList[index].product.price
+                                      .toString())
                                 ],
-                              )
-                            ],
-                          ),
+                              ),
+                            ),
+                            Spacer(),
+                            Column(
+                              mainAxisSize: MainAxisSize.min,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                InkWell(
+                                    onTap: () {
+                                      providerObj.deletFormCart(index);
+                                    },
+                                    child: Icon(Icons.delete)),
+                                SizedBox(
+                                  height: 20,
+                                ),
+                                Row(
+                                  children: [
+                                    InkWell(
+                                        onTap: () {
+                                          providerObj.onIncrementQty(index);
+                                        },
+                                        child: Icon(Icons.add)),
+                                    Center(
+                                      child: Text(providerObj
+                                          .cartList[index].qty
+                                          .toString()),
+                                    ),
+                                    InkWell(
+                                        onTap: () {
+                                          providerObj.onDecrementQty(index);
+                                        },
+                                        child: Icon(Icons.remove))
+                                  ],
+                                )
+                              ],
+                            )
+                          ],
                         ),
                       ),
-                    ));
-          },
-        ));
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: InkWell(
+                  onTap: () {},
+                  child: Container(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 30, vertical: 12),
+                      child: Text(
+                        providerObj.totalAmount.toStringAsFixed(2),
+                        style: TextStyle(
+                            color: Colors.black, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    decoration: BoxDecoration(
+                        border: Border.all(),
+                        borderRadius: BorderRadius.circular(40)),
+                  ),
+                ),
+              )
+            ],
+          );
+        },
+      ),
+    );
   }
 }
